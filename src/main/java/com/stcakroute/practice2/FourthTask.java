e7291ff3825285b2bdaf3134dfd1c6bd39d941dc
sureshkumar007 <sureshkumar12074@gmail.com>
2019-07-11 15:44:35 +0530

    Commit directory..

---
+++ src/main/java/com/stcakroute/practice2/FourthTask.java
@@ -0,0 +1,22 @@
+package com.stcakroute.practice;
+import java.util.*;
+public class FourthTask
+{
+    public static void main(String args[])
+        {
+            Scanner input=new Scanner(System.in);
+            {
+                int number;
+                System.out.println("Enter the number");
+                number=input.nextInt();
+                for(int i=1;i<=number;i++)
+                {
+                    for(int j=1;j<=i;j++)
+                    {
+                        System.out.print(" "+i);
+                    }
+                }
+            }
+    }
+
+}
